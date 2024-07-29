a4c4f15413c2c2182903b952b45ba57a189b54ed
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2024-07-29 16:55:36 +0530

    Update server.js

--- Url Shortner/server.js
+++ Url Shortner/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors()); // Enable CORS for all routes
