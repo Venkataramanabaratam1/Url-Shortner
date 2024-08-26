6a1e43ce75f7ab23958c57b6d5b99b1b6db7c6d1
VENKATARAMANA BARATAM <109843726+Venkataramanabaratam1@users.noreply.github.com>
2024-08-26 08:05:20 +0530

    Update server.js

--- UrlShortner/server.js
+++ UrlShortner/server.js
@@ -29,6 +29,10 @@ app.use(express.static('public'));
 
 path.join(__dirname,'public')
 
+app.get('/',(req,res)=>{
+    res.send('index.html')
+})
+
 // Function to generate short URL using crypto
 function generateShortUrl(length) {
     return crypto.randomBytes(length).toString('hex').slice(0, length);
